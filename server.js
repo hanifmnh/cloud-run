0403bbd8619833ead18aaf7a703a804c40d67dd4
Nur Hanif <72727969+hanifmnh@users.noreply.github.com>
2023-03-14 08:48:41 +0700

    Minor changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send('Goodbye from Cloud Run!');
+	res.send('Prak IF-C');
 });
 
 const PORT = process.env.PORT || 8080;
